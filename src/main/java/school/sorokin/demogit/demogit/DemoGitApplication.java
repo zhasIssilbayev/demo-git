0d8d2f6292fed9104475f4368d30a8c687359987
zhasIsilbayev <zhas240299@gmail.com>
2025-09-23 22:10:31 +0500

    init commit

---
+++ src/main/java/school/sorokin/demogit/demogit/DemoGitApplication.java
@@ -0,0 +1,13 @@
+package school.sorokin.demogit.demogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/school/sorokin/demogit/demogit/DemoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package school.sorokin.demogit.demogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
